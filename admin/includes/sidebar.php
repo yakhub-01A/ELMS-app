1c2d6a4272f47eba988d90ce8d489bdce1c168c7
yakhub-01A <43907751+yakhub-01A@users.noreply.github.com>
2021-06-24 19:44:38 +0530

    remove icons

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -10,9 +10,9 @@
              </div>
 
              <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
-                 <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><i class="material-icons">settings_input_svideo</i>Dashboard</a></li>
+                 <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php">Dashboard</a></li>
                  <li class="no-padding">
-                     <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">apps</i>Department<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                     <a class="collapsible-header waves-effect waves-grey">Department</a>
                      <div class="collapsible-body">
                          <ul>
                              <li><a href="adddepartment.php">Add Department</a></li>
@@ -21,7 +21,7 @@
                      </div>
                  </li>
                  <li class="no-padding">
-                     <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">code</i>Leave Type<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                     <a class="collapsible-header waves-effect waves-grey">Leave Type</a>
                      <div class="collapsible-body">
                          <ul>
                              <li><a href="addleavetype.php">Add Leave Type</a></li>
@@ -30,7 +30,7 @@
                      </div>
                  </li>
                  <li class="no-padding">
-                     <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>Employees<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                     <a class="collapsible-header waves-effect waves-grey">Employees</a>
                      <div class="collapsible-body">
                          <ul>
                              <li><a href="addemployee.php">Add Employee</a></li>
@@ -41,7 +41,7 @@
                  </li>
 
                  <li class="no-padding">
-                     <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Leave Management<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                     <a class="collapsible-header waves-effect waves-grey">Leave Management</a>
                      <div class="collapsible-body">
                          <ul>
                              <li><a href="leaves.php">All Leaves </a></li>
@@ -54,10 +54,10 @@
                  </li>
 
 
-                 <li class="no-padding"><a class="waves-effect waves-grey" href="changepassword.php"><i class="material-icons">settings_input_svideo</i>Chnage Password</a></li>
+                 <li class="no-padding"><a class="waves-effect waves-grey" href="changepassword.php">Chnage Password</a></li>
 
                  <li class="no-padding">
-                     <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Log Out</a>
+                     <a class="waves-effect waves-grey" href="logout.php">Log Out</a>
                  </li>
 
 
