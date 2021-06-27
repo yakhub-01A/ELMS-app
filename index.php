a3628134941fc9060069b3f9a134fdd955fd8f01
yakhub-01A <43907751+yakhub-01A@users.noreply.github.com>
2021-06-27 09:19:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -98,11 +98,11 @@ if (isset($_POST['signin'])) {
                                 <div class="row">
                                     <form class="col col-md-12 " name="signin" method="post">
                                         <div class="input-field col s12">
-                                            <input id="username" type="text" name="username" class="validate" autocomplete="off" required>
+                                            <input id="username" type="text" name="username" class="validate" autocomplete="none" required>
                                             <label for="email">Enter Registered Email Id</label>
                                         </div>
                                         <div class="input-field col col-md-12">
-                                            <input id="password" type="password" class="validate" name="password" autocomplete="off" required>
+                                            <input id="password" type="password" class="validate" name="password" autocomplete="none" required>
                                             <label for="password">Enter Password</label>
                                         </div>
                                         <div class="col col-md-12 center m-t-sm">
